4ae004b930e040c38961e1dbd34cc590eae42934
Soda Can <103301261+ytsodacan@users.noreply.github.com>
2024-08-24 16:01:18 +1000

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1724478927,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tilemap.png",
+		"images/spr_player-sheet0.png",
+		"images/spr_player-sheet1.png",
+		"images/spr_player-sheet2.png",
+		"images/tilemap2.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
